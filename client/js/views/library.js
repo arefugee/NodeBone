e982c9b5bb0f65db5cadb7db49a6b03508374cb1
phoenixbox <shane@quickleft.com>
2014-02-21 14:11:33 -0700

    added an add book event on click of the add button which parses the form children el inputs and uses those values to create a new book model which is added to the collection added a listener to the library view which renders the new book view on collection add event

--- client/js/views/library.js
+++ client/js/views/library.js
@@ -2,8 +2,14 @@ var app = app || {};
 
 app.LibraryView = Backbone.View.extend({
   el: '#books',
+
+  events: {
+    'click #add': 'addBook'
+  },
+
   initialize: function(initialBooks){
     this.collection = new app.Library(initialBooks);
+    this.listenTo(this.collection, 'add', this.renderBook);
     this.render();
   },
 
@@ -19,5 +25,21 @@ app.LibraryView = Backbone.View.extend({
       model: book
     });
     this.$el.append(bookView.render().el);
+  },
+
+  addBook: function(event){
+    event.preventDefault();
+
+    // Cache input vals in a hash
+    var formData = {};
+
+    // Create new models
+    $('#addBook div').children('input').each(function(i,el){
+      if($(el).val() !== ''){
+        formData[el.id] = $(el).val();
+      }
+    });
+    // Create model instance and add to the collection
+    this.collection.add(new app.Book(formData));
   }
 });
