12e8d0b4fc5ede2dca087e7bebe0bf6de6c1d8b2
phoenixbox <shane@quickleft.com>
2014-02-22 11:35:34 -0700

    parse the _id attribute on the repsonse to set it on the model

--- client/js/models/book.js
+++ client/js/models/book.js
@@ -7,5 +7,10 @@ app.Book = Backbone.Model.extend({
     author: 'Author',
     releaseDate: 'ReleaseDate',
     keywords: 'None'
+  },
+
+  parse: function(response){
+    response.id = response._id;
+    return response;
   }
 });
